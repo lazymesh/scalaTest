fe103428a6b1471b853ac68bd584daa4b0cfb400
anahcolus <lazymesh@gmail.com>
2017-02-07 06:25:01 +0545

    master table diagnosis ongoing

--- src/main/scala/officework/doingWithClasses/masterTableUsingDF/MasterTableDiagnosisGroupers.scala
+++ src/main/scala/officework/doingWithClasses/masterTableUsingDF/MasterTableDiagnosisGroupers.scala
@@ -1,6 +1,7 @@
 package main.scala.officework.doingWithClasses.masterTableUsingDF
 
 import main.scala.officework.doingWithClasses.MasterTableProperties
+import org.apache.spark.sql.{DataFrame, Row}
 
 import scala.collection.immutable.HashMap
 import scala.io.Source
@@ -43,18 +44,53 @@ class MasterTableDiagnosisGroupers extends scala.Serializable{
           diagGrouperId = line(5).replaceAll("\"", "")
           diagGrouperDesc = line(6).replaceAll("\"", "")
           if (!diagCode.isEmpty) {
-            if (diagGrouperDesc.isEmpty) diagGrouperDesc = diagSupGrouperDesc
-            diagCode_diagGrouperIdMap ++= HashMap(diagCode -> diagGrouperId)
+            if (diagGrouperDesc.isEmpty){ diagGrouperDesc = diagSupGrouperDesc}
+            addDiagCodeToDiagGrouperId(diagCode, diagGrouperId)
             if(!diagGrouperId_diagGrouperDescMap.contains(diagGrouperId)) {
-              diagGrouperId_diagGrouperDescMap ++= HashMap(diagGrouperId -> diagGrouperDesc)
+              addDiagGrouperIdToDiagGrouperDesc(diagGrouperId, diagGrouperDesc)
             }
-            diagCode_diagSuperGrouperIdMap ++= HashMap(diagCode -> diagSupGrouperId)
+            addDiagCodeToDiagSuperGrouperId(diagCode, diagSupGrouperId)
             if (!diagSuperGrouperId_diagSuperGrouperDescMap.contains(diagSupGrouperId)) {
-              diagSuperGrouperId_diagSuperGrouperDescMap ++= HashMap(diagSupGrouperId -> diagSupGrouperDesc)
+              addSuperGrouperIdToSuperGrouperDesc(diagSupGrouperId, diagSupGrouperDesc)
             }
           }
       })
     }
+  }
+
+  def readPropertiesToMap(masterTableDF : DataFrame): Any = {
+    var diagCode: String = ""
+    var diagSupGrouperId: String = ""
+    var diagSupGrouperDesc: String = ""
+    var diagGrouperId: String = ""
+    var diagGrouperDesc: String = ""
+
+    masterTableDF.show
+    def getValues(row: Row, names: Seq[String]) = names.map(
+      name => println(name+" "+row.getAs[Any](name))
+    )
+
+    val names = Seq("diagnosisCode", "grouperID", "grouperDescription", "superGrouperID", "superGrouperDescription")
+    masterTableDF.rdd.map(getValues(_, names))
+
+/*    masterTableDF.map(line =>{
+      diagCode = line(1).replaceAll("\"", "")
+      diagSupGrouperId = line(3).replaceAll("\"", "")
+      diagSupGrouperDesc = line(4).replaceAll("\"", "")
+      diagGrouperId = line(5).replaceAll("\"", "")
+      diagGrouperDesc = line(6).replaceAll("\"", "")
+      if (!diagCode.isEmpty) {
+        if (diagGrouperDesc.isEmpty){ diagGrouperDesc = diagSupGrouperDesc}
+        addDiagCodeToDiagGrouperId(diagCode, diagGrouperId)
+        if(!diagGrouperId_diagGrouperDescMap.contains(diagGrouperId)) {
+          addDiagGrouperIdToDiagGrouperDesc(diagGrouperId, diagGrouperDesc)
+        }
+        addDiagCodeToDiagSuperGrouperId(diagCode, diagSupGrouperId)
+        if (!diagSuperGrouperId_diagSuperGrouperDescMap.contains(diagSupGrouperId)) {
+          addSuperGrouperIdToSuperGrouperDesc(diagSupGrouperId, diagSupGrouperDesc)
+        }
+      }
+    })*/
   }
 
   def validateDiagnosisMasterTable(lineLength : Int, version : String): Boolean = {
@@ -63,6 +99,22 @@ class MasterTableDiagnosisGroupers extends scala.Serializable{
     if (!masterTableProperties.getMasterTableDiagVersion().equalsIgnoreCase(version))
       throw new RuntimeException("Did not get correct versions's Diagnosis Master Table:" + " Expected Version: " + masterTableProperties.getMasterTableDiagVersion() + " but got " + version)
     true
+  }
+
+  def addDiagCodeToDiagGrouperId(diagCode:String, diagGrouperId: String): Any = {
+    diagCode_diagGrouperIdMap ++= HashMap(diagCode -> diagGrouperId)
+  }
+
+  def addDiagGrouperIdToDiagGrouperDesc(diagGrouperId: String, diagGrouperDesc:String): Any = {
+    diagGrouperId_diagGrouperDescMap ++= HashMap(diagGrouperId -> diagGrouperDesc)
+  }
+
+  def addDiagCodeToDiagSuperGrouperId(diagCode: String, diagSupGrouperId: String): Any = {
+    diagCode_diagSuperGrouperIdMap ++= HashMap(diagCode -> diagSupGrouperId)
+  }
+
+  def addSuperGrouperIdToSuperGrouperDesc(diagSupGrouperId: String, diagSupGrouperDesc: String): Any = {
+    diagSuperGrouperId_diagSuperGrouperDescMap ++= HashMap(diagSupGrouperId -> diagSupGrouperDesc)
   }
 
   def getDiagCodeToDiagGrouperId(): HashMap[String, String] = {
--- src/test/MasterTablePropertiesTests.scala
+++ src/test/MasterTablePropertiesTests.scala
@@ -2,7 +2,7 @@ package test
 
 import main.scala.officework.doingWithClasses._
 import main.scala.officework.doingWithClasses.masterTableUsingDF.{DiagnosisMasterTableUDFs, MasterTableDiagnosisGroupers}
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.scalatest.{BeforeAndAfterEach, FunSuite}
 
 import scala.io.Source
@@ -38,47 +38,19 @@ class MasterTablePropertiesTests extends FunSuite with BeforeAndAfterEach {
     println(firstLine.split("\\|", -1).length)
   }
 
-  test("testing MasterTableDiagnosisGroupers"){
+  test("testing master table by creating hashmap"){
     val masterTableDiagnosisGroupers = new MasterTableDiagnosisGroupers
     val temp = masterTableDiagnosisGroupers.readPropertiesToMap("/Diagnosis.csv")
     println(masterTableDiagnosisGroupers.getDiagCodeToDiagGrouperId())
   }
 
-  test("testing masterTable on Medical Table"){
+  test("1 testing masterTable on Medical Table"){
     val sparkContext = sparkSession.sparkContext
-    val sqlContext = sparkSession.sqlContext
-    import sqlContext.implicits._
-    var medicalDiags = Seq(("E944.3", "", "M10.139", "M46.32",
-      "diag1_grouper_id", "diag1_grouper_desc", "diag1_supergrouper_id", "diag1_supergrouper_desc",
-      "diag2_grouper_id", "diag2_grouper_desc", "diag2_supergrouper_id", "diag2_supergrouper_desc",
-      "diag3_grouper_id", "diag3_grouper_desc", "diag3_supergrouper_id", "diag3_supergrouper_desc",
-      "diag4_grouper_id", "diag4_grouper_desc", "diag4_supergrouper_id", "diag4_supergrouper_desc"),
-      ("M46.32", "820.32", "E13.341", "M10.139",
-        "diag1_grouper_id", "diag1_grouper_desc", "diag1_supergrouper_id", "diag1_supergrouper_desc",
-        "diag2_grouper_id", "diag2_grouper_desc", "diag2_supergrouper_id", "diag2_supergrouper_desc",
-        "diag3_grouper_id", "diag3_grouper_desc", "diag3_supergrouper_id", "diag3_supergrouper_desc",
-        "diag4_grouper_id", "diag4_grouper_desc", "diag4_supergrouper_id", "diag4_supergrouper_desc"),
-      ("M10.139", "M22.90", "820.32", "",
-        "diag1_grouper_id", "diag1_grouper_desc", "diag1_supergrouper_id", "diag1_supergrouper_desc",
-        "diag2_grouper_id", "diag2_grouper_desc", "diag2_supergrouper_id", "diag2_supergrouper_desc",
-        "diag3_grouper_id", "diag3_grouper_desc", "diag3_supergrouper_id", "diag3_supergrouper_desc",
-        "diag4_grouper_id", "diag4_grouper_desc", "diag4_supergrouper_id", "diag4_supergrouper_desc"),
-      ("E944.3", "M22.90", "E13.341", "D21.0",
-        "diag1_grouper_id", "diag1_grouper_desc", "diag1_supergrouper_id", "diag1_supergrouper_desc",
-        "diag2_grouper_id", "diag2_grouper_desc", "diag2_supergrouper_id", "diag2_supergrouper_desc",
-        "diag3_grouper_id", "diag3_grouper_desc", "diag3_supergrouper_id", "diag3_supergrouper_desc",
-        "diag4_grouper_id", "diag4_grouper_desc", "diag4_supergrouper_id", "diag4_supergrouper_desc"))
-      .toDF("svc_diag_1_code", "svc_diag_2_code", "svc_diag_3_code", "svc_diag_4_code",
-        "diag1_grouper_id", "diag1_grouper_desc", "diag1_supergrouper_id", "diag1_supergrouper_desc",
-        "diag2_grouper_id", "diag2_grouper_desc", "diag2_supergrouper_id", "diag2_supergrouper_desc",
-        "diag3_grouper_id", "diag3_grouper_desc", "diag3_supergrouper_id", "diag3_supergrouper_desc",
-        "diag4_grouper_id", "diag4_grouper_desc", "diag4_supergrouper_id", "diag4_supergrouper_desc")
+    var medicalDiags = medicalDataCreation
 
     val masterTableDiagnosisGroupers = new MasterTableDiagnosisGroupers
     val temp = masterTableDiagnosisGroupers.readPropertiesToMap("/Diagnosis.csv")
-//    println(masterTableDiagnosisGroupers.getDiagCodeToDiagGrouperId())
     val broadCastedDiagMT = sparkContext.broadcast(masterTableDiagnosisGroupers)
-
     val diagnosisMasterTableUDFs = new DiagnosisMasterTableUDFs(broadCastedDiagMT)
     for(i <- 1 to 4) {
       medicalDiags = medicalDiags.withColumn("diag"+i+"_grouper_id", diagnosisMasterTableUDFs.grouperId(medicalDiags("svc_diag_"+i+"_code")))
@@ -86,13 +58,11 @@ class MasterTablePropertiesTests extends FunSuite with BeforeAndAfterEach {
         .withColumn("diag"+i+"_supergrouper_id", diagnosisMasterTableUDFs.superGrouperId(medicalDiags("svc_diag_"+i+"_code")))
         .withColumn("diag"+i+"_supergrouper_desc", diagnosisMasterTableUDFs.superGrouperIdDesc(medicalDiags("svc_diag_"+i+"_code")))
     }
-
     medicalDiags.show
-
     sparkContext.stop()
   }
 
-  test("testing master table by creating dataframes"){
+  test("2 testing master table by creating dataframes"){
     val sparkContext = sparkSession.sparkContext
     val sqlContext = sparkSession.sqlContext
 
@@ -100,11 +70,52 @@ class MasterTablePropertiesTests extends FunSuite with BeforeAndAfterEach {
     val masterTableSchema = generateSchemas.dynamicSchema("/diagnosisLayout.csv")
     val generateDataFrame = new GenerateDataFrame
 
-    val masterTableDiagRdd = sparkContext.textFile("/home/ramaharjan/Documents/testProjects/gitHubScala/scalaTest/src/main/resources/Diagnosis.csv")
+    val masterTableDiagRdd = sparkContext.textFile("/home/anahcolus/IdeaProjects/scalaTest/src/main/resources/Diagnosis.csv")
     val masterTableDataFrame = generateDataFrame.createMasterDataFrame(sqlContext, masterTableDiagRdd, masterTableSchema)
 
-    import sqlContext.implicits._
-    var medicalDiags = Seq(("E944.3", "", "M10.139", "M46.32",
+    var medicalDiags = medicalDataCreation
+    val broadCastedDiagMT = sparkContext.broadcast(masterTableDataFrame)
+    val masterTableUdfs = new MasterTableUdfs(broadCastedDiagMT)
+    for(i <- 1 to 4) {
+      medicalDiags = medicalDiags.withColumn("diag"+i+"_grouper_id", masterTableUdfs.getDiagGrouperId(medicalDiags("svc_diag_"+i+"_code")))
+        .withColumn("diag"+i+"_grouper_desc", masterTableUdfs.getDiagGrouperIdDesc(medicalDiags("svc_diag_"+i+"_code")))
+        .withColumn("diag"+i+"_supergrouper_id", masterTableUdfs.getSuperDiagGrouperId(medicalDiags("svc_diag_"+i+"_code")))
+        .withColumn("diag"+i+"_supergrouper_desc", masterTableUdfs.getsuperDiagGrouperIdDesc(medicalDiags("svc_diag_"+i+"_code")))
+    }
+    medicalDiags.show
+    sparkContext.stop()
+  }
+
+  test("3 combining 1 hashmap and 2 dataframe"){
+    val sparkContext = sparkSession.sparkContext
+    val sqlContext = sparkSession.sqlContext
+
+    val generateSchemas = new GenerateSchemas
+    val masterTableSchema = generateSchemas.dynamicSchema("/diagnosisLayout.csv")
+    val generateDataFrame = new GenerateDataFrame
+
+    val masterTableDiagRdd = sparkContext.textFile("/home/anahcolus/IdeaProjects/scalaTest/src/main/resources/Diagnosis.csv")
+    val masterTableDataFrame = generateDataFrame.createMasterDataFrame(sqlContext, masterTableDiagRdd, masterTableSchema)
+
+    var medicalDiags = medicalDataCreation
+    val masterTableDiagnosisGroupers = new MasterTableDiagnosisGroupers
+    val temp = masterTableDiagnosisGroupers.readPropertiesToMap(masterTableDataFrame)
+    val broadCastedDiagMT = sparkContext.broadcast(masterTableDiagnosisGroupers)
+    val diagnosisMasterTableUDFs = new DiagnosisMasterTableUDFs(broadCastedDiagMT)
+    for(i <- 1 to 4) {
+      medicalDiags = medicalDiags.withColumn("diag"+i+"_grouper_id", diagnosisMasterTableUDFs.grouperId(medicalDiags("svc_diag_"+i+"_code")))
+        .withColumn("diag"+i+"_grouper_desc", diagnosisMasterTableUDFs.grouperIdDesc(medicalDiags("svc_diag_"+i+"_code")))
+        .withColumn("diag"+i+"_supergrouper_id", diagnosisMasterTableUDFs.superGrouperId(medicalDiags("svc_diag_"+i+"_code")))
+        .withColumn("diag"+i+"_supergrouper_desc", diagnosisMasterTableUDFs.superGrouperIdDesc(medicalDiags("svc_diag_"+i+"_code")))
+    }
+    medicalDiags.show
+    sparkContext.stop()
+  }
+
+  def medicalDataCreation : DataFrame = {
+    val sQLContext = sparkSession.sqlContext
+    import sQLContext.implicits._
+    val medicalSequenceData = Seq(("E944.3", "", "M10.139", "M46.32",
       "diag1_grouper_id", "diag1_grouper_desc", "diag1_supergrouper_id", "diag1_supergrouper_desc",
       "diag2_grouper_id", "diag2_grouper_desc", "diag2_supergrouper_id", "diag2_supergrouper_desc",
       "diag3_grouper_id", "diag3_grouper_desc", "diag3_supergrouper_id", "diag3_supergrouper_desc",
@@ -129,20 +140,7 @@ class MasterTablePropertiesTests extends FunSuite with BeforeAndAfterEach {
         "diag2_grouper_id", "diag2_grouper_desc", "diag2_supergrouper_id", "diag2_supergrouper_desc",
         "diag3_grouper_id", "diag3_grouper_desc", "diag3_supergrouper_id", "diag3_supergrouper_desc",
         "diag4_grouper_id", "diag4_grouper_desc", "diag4_supergrouper_id", "diag4_supergrouper_desc")
-
-    val broadCastedDiagMT = sparkContext.broadcast(masterTableDataFrame)
-
-    val masterTableUdfs = new MasterTableUdfs(broadCastedDiagMT)
-    for(i <- 1 to 4) {
-      medicalDiags = medicalDiags.withColumn("diag"+i+"_grouper_id", masterTableUdfs.getDiagGrouperId(medicalDiags("svc_diag_"+i+"_code")))
-        .withColumn("diag"+i+"_grouper_desc", masterTableUdfs.getDiagGrouperIdDesc(medicalDiags("svc_diag_"+i+"_code")))
-        .withColumn("diag"+i+"_supergrouper_id", masterTableUdfs.getSuperDiagGrouperId(medicalDiags("svc_diag_"+i+"_code")))
-        .withColumn("diag"+i+"_supergrouper_desc", masterTableUdfs.getsuperDiagGrouperIdDesc(medicalDiags("svc_diag_"+i+"_code")))
-    }
-
-    medicalDiags.show
-
-    sparkContext.stop()
+    medicalSequenceData
   }
 
 }
